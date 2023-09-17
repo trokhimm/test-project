6ceba64e8942270df211c5d4013855d70c2c32e1
trokhimm <trokhim.m@gmail.com>
2023-09-17 18:18:42 +0100

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+
+
+//const results = confirm ("Are you here");
+//console.log (results);
+
+// const answer = +prompt ('Вам уже есть 18?', ' ');
+// console.log (answer+5);
+
+// const answers = [];
+
+// answers[0] = prompt ('Как Ваше імя', ' ');
+// answers[1] = prompt ('Как Ваше фамилия', ' ');
+// answers[2] = prompt ('Сколько вам лет', ' ');
+// //console.log (answers);
+// document.write (answers);
+
+"use strict";
+
+let incr = 10,
+    decr = 5;
+
+    incr++;
+    decr--;
+
+    console.log(incr);
+    console.log(decr);
